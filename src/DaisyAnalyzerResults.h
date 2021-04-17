167354c6ffc52c329ab86669c15577fd3b9ebfff
Willy Mills <orwonthe@gmail.com>
2021-04-17 14:23:00 -0600

    First version, just getting started
    Runs enough to display settings.

---
+++ src/DaisyAnalyzerResults.h
@@ -0,0 +1,31 @@
+#ifndef SPI_ANALYZER_RESULTS
+#define SPI_ANALYZER_RESULTS
+
+#include <AnalyzerResults.h>
+
+#define SPI_ERROR_FLAG ( 1 << 0 )
+
+class DaisyAnalyzer;
+class DaisyAnalyzerSettings;
+
+class DaisyAnalyzerResults : public AnalyzerResults
+{
+public:
+	DaisyAnalyzerResults(DaisyAnalyzer* analyzer, DaisyAnalyzerSettings* settings );
+	virtual ~DaisyAnalyzerResults();
+
+	virtual void GenerateBubbleText( U64 frame_index, Channel& channel, DisplayBase display_base );
+	virtual void GenerateExportFile( const char* file, DisplayBase display_base, U32 export_type_user_id );
+
+	virtual void GenerateFrameTabularText(U64 frame_index, DisplayBase display_base );
+	virtual void GeneratePacketTabularText( U64 packet_id, DisplayBase display_base );
+	virtual void GenerateTransactionTabularText( U64 transaction_id, DisplayBase display_base );
+
+protected: //functions
+
+protected:  //vars
+	DaisyAnalyzerSettings* mSettings;
+	DaisyAnalyzer* mAnalyzer;
+};
+
+#endif //SPI_ANALYZER_RESULTS
---
+++ src/DaisyAnalyzerSettings.h
@@ -0,0 +1,42 @@
+#ifndef DAISY_ANALYZER_SETTINGS
+#define DAISY_ANALYZER_SETTINGS
+
+#include <AnalyzerSettings.h>
+#include <AnalyzerTypes.h>
+
+class DaisyAnalyzerSettings : public AnalyzerSettings
+{
+public:
+	DaisyAnalyzerSettings();
+	virtual ~DaisyAnalyzerSettings();
+
+	virtual bool SetSettingsFromInterfaces();
+	virtual void LoadSettings( const char* settings );
+	virtual const char* SaveSettings();
+
+	void UpdateInterfacesFromSettings();
+	
+	Channel mMosiChannel;
+	Channel mMisoChannel;
+	Channel mClockChannel;
+	Channel mEnableChannel;
+	AnalyzerEnums::ShiftOrder mShiftOrder;
+	U32 mBitsPerTransfer;
+	BitState mClockInactiveState;
+	AnalyzerEnums::Edge mDataValidEdge;
+	BitState mEnableActiveState;
+
+
+protected:
+	std::auto_ptr< AnalyzerSettingInterfaceChannel >	mMosiChannelInterface;
+	std::auto_ptr< AnalyzerSettingInterfaceChannel >	mMisoChannelInterface;
+	std::auto_ptr< AnalyzerSettingInterfaceChannel >	mClockChannelInterface;
+	std::auto_ptr< AnalyzerSettingInterfaceChannel >	mEnableChannelInterface;
+	std::auto_ptr< AnalyzerSettingInterfaceNumberList > mShiftOrderInterface;
+	std::auto_ptr< AnalyzerSettingInterfaceNumberList > mBitsPerTransferInterface;
+	std::auto_ptr< AnalyzerSettingInterfaceNumberList > mClockInactiveStateInterface;
+	std::auto_ptr< AnalyzerSettingInterfaceNumberList > mDataValidEdgeInterface;
+	std::auto_ptr< AnalyzerSettingInterfaceNumberList > mEnableActiveStateInterface;
+};
+
+#endif //DAISY_ANALYZER_SETTINGS
